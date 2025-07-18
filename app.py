f19a9faf383bfd354c3670708ba82f3a437462a1
Muzammil Hameed <134141927+Muzammil-Hameed@users.noreply.github.com>
2025-07-18 22:05:25 +0500

    Update app.py

--- app.py
+++ app.py
@@ -70,4 +70,6 @@ def predict():
     return render_template("index.html", error="Unsupported file type. Please upload JPG or PNG.")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
